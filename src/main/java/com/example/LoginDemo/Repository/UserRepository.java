f3f62388c1f75062f547c9178bed33247e61baa9
devrajyo <138962442+devrajyo@users.noreply.github.com>
2025-03-27 23:40:32 +0530

    added  findByFirstname(String firstname) in UserRepository

--- src/main/java/com/example/LoginDemo/Repository/UserRepository.java
+++ src/main/java/com/example/LoginDemo/Repository/UserRepository.java
@@ -10,5 +10,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByUsername(String username);
 
-
+    Optional<UserEntity> findByFirstname(String firstname);
 }
